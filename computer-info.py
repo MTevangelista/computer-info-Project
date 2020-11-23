61ae1ee28cb721d59ed58714ed4475ae622f3513
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-11-22 21:38:42 -0300

    add a graphic indication of the percentage of disk usage

--- computer-info.py
+++ computer-info.py
@@ -6,6 +6,7 @@ import matplotlib.patches as mpatches
 option = input(
     'Digite 1 para visualizar o gráfico da porcentagem do uso de memória:\n'
     'Digite 2 para visualizar o gráfico da porcentagem do uso de CPU:\n'
+    'Digite 3 para visualizar o gráfico da porcentagem do uso de disco:\n'
 )
 
 plt.title("Monitoramento e Análise do Computador")
@@ -21,7 +22,7 @@ def show_memory_usage_graph():
     
     plt.show()
 
-def show_cpu_graph():
+def show_cpu_usage_graph():
     values = []
     for i in range(5):
         cpu = psutil.cpu_percent(interval=1)
@@ -30,9 +31,20 @@ def show_cpu_graph():
     plt.plot(values)
     plt.show()
 
+def show_disk_usage_graph():
+    values = []
+    for i in range(5):
+        disk = psutil.disk_usage('.')
+        values.append(disk)
+    
+    plt.plot(values)
+    plt.show()
+
 if int(option) == 1:
     show_memory_usage_graph()
 elif int(option) == 2:
-    show_cpu_graph()
+    show_cpu_usage_graph()
+elif int(option) == 3:
+    show_disk_usage_graph()
 else:
     print("Opção inválida")
