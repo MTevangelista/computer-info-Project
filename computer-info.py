9c2ef67cbcc0f2a7ec28571aa4f9cf56cf674b8d
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-11-22 21:35:45 -0300

    add a graphic indication of the percentage of CPU usage

--- computer-info.py
+++ computer-info.py
@@ -1,9 +1,11 @@
 import psutil
 import numpy as np 
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 option = input(
     'Digite 1 para visualizar o gráfico da porcentagem do uso de memória:\n'
+    'Digite 2 para visualizar o gráfico da porcentagem do uso de CPU:\n'
 )
 
 plt.title("Monitoramento e Análise do Computador")
@@ -19,8 +21,18 @@ def show_memory_usage_graph():
     
     plt.show()
 
+def show_cpu_graph():
+    values = []
+    for i in range(5):
+        cpu = psutil.cpu_percent(interval=1)
+        values.append(cpu)
+
+    plt.plot(values)
+    plt.show()
 
 if int(option) == 1:
     show_memory_usage_graph()
+elif int(option) == 2:
+    show_cpu_graph()
 else:
     print("Opção inválida")
